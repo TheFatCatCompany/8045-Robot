9acd1daf7197245f26a1d2f378886c552be11475
TheFatCatCompany <iansho317@gmail.com>
2020-01-13 17:06:10 -0800

    Update Drivetrain.java

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -10,18 +10,29 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drivetrain extends SubsystemBase {
   /**
    * Creates a new ExampleSubsystem.
    */
-  public Drivetrain(Joystick J, VictorSPX V1) {
+  private Joystick j;
+  private VictorSPX v1, v2, v3, v4;
+  public Drivetrain(DifferentialDrive m_myRobot, Joystick J, VictorSPX V1, VictorSPX V2, VictorSPX V3, VictorSPX V4) 
+  {
+   j = J;
+   v1 = V1;
+   v2 = V2;
+   v3 = V3;
+   v4 = V4;
+   m_myRobot = new DifferentialDrive(V1, V2, V3, V4);   
 
   }
-
   @Override
   public void periodic() {
+
+
     // This method will be called once per scheduler run
   }
 }
