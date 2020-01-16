0bb28cceb1eaa13e5b7a7cdffd3ae94adb6a9b5c
Andrew I <andrewing777@gmail.com>
2020-01-15 17:46:18 -0800

    Updated Move.java

--- src/main/java/frc/robot/commands/Move.java
+++ src/main/java/frc/robot/commands/Move.java
@@ -6,11 +6,9 @@ import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class Moveforward extends CommandBase
+public class Move extends CommandBase
 {
     private Joystick j;
-    private WPI_VictorSPX leftSlave = new WPI_VictorSPX(1);
-    private WPI_VictorSPX rightSlave = new WPI_VictorSPX(3);
 
 // Called when the command is initially scheduled.
   @Override
@@ -20,12 +18,13 @@ public class Moveforward extends CommandBase
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-move();  
-}
+    Drivetrain.m_myRobot.tankDrive(j.getX(),j.getY());
+  }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+  Drivetrain.m_myRobot.tankDrive(0, 0);
   }
 
   // Returns true when the command should end.
@@ -34,10 +33,7 @@ move();
     return false;
   }
     
-  public void move()
-    {
-        Drivetrain.m_myRobot.tankDrive(j.getX(),j.getY());
-    }
+  
 
 
 }
