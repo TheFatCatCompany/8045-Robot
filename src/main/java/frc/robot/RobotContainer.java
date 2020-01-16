99876895b0db0ae14e1d4c84202f28f9142a0b65
Andrew I <andrewing777@gmail.com>
2020-01-15 18:54:21 -0800

    Reworked drivetrain and Move. Also  added constants.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
@@ -22,7 +23,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
+  public Joystick playercontroller = new Joystick(Constants.joystick);
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
 
@@ -33,6 +34,7 @@ public class RobotContainer {
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
+  
   }
 
   /**
--- src/main/java/frc/robot/commands/Move.java
+++ src/main/java/frc/robot/commands/Move.java
@@ -1,14 +1,19 @@
 package frc.robot.commands;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-import frc.robot.subsystems.Drivetrain;
-
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Robot;
+import frc.robot.subsystems.DriveTrain;
 
-public class Move extends CommandBase
+public class Move extends Command
 {
-    private Joystick j;
+    public Move() {
+          Joystick j;
+          // requires(Robot.m_drivetrain);
+
+    }
 
 // Called when the command is initially scheduled.
   @Override
@@ -18,13 +23,13 @@ public class Move extends CommandBase
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    Drivetrain.m_myRobot.tankDrive(j.getX(),j.getY());
+    DriveTrain.m_myRobot.tankDrive(j.getX(),j.getY());
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-  Drivetrain.m_myRobot.tankDrive(0, 0);
+  public void end() {
+  DriveTrain.m_myRobot.tankDrive(0, 0);
   }
 
   // Returns true when the command should end.
