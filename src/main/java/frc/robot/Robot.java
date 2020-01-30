a456b5192fd5d09f40dc8585906a03d2362f3480
zslrmhb <zslrmhb@gmail.com>
2020-01-29 16:31:13 -0800

    joystick pickup changes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -94,11 +94,15 @@ public class Robot extends TimedRobot {
     // commands, running already-scheduled commands, removing finished or interrupted commands,
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work. 
-    if(RobotContainer.joystick.getRawButton(3)){
+    if (RobotContainer.joystick.getRawButton(3)){
       RobotContainer.pickup.execute();
+    }
+    else if (!RobotContainer.joystick.getRawButton(3)) {
+      RobotContainer.pickup.isFinished();
     }
     CommandScheduler.getInstance().run();
   }
+
 
   /**
    * This function is called once each time the robot enters Disabled mode.
